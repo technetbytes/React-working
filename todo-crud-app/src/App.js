498749adfde230cdfe7ec80fe61a73f33043d4b7
technetbytes <saqibullah@gmail.com>
2019-02-13 12:10:20 +0500

    remove png image

--- todo-crud-app/src/App.js
+++ todo-crud-app/src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src="logo.svg" width="30" height="30" alt="CodingTheSmartWay.com" />
+              <img src="logo.svg" width="30" height="30" alt="CaaSHub.com" />
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Tosdo</Link>
+                  <Link to="/" className="nav-link">Todo</Link>
                 </li>
                 <li className="navbar-item">
                   <Link to="/create" className="nav-link">Create Todos</Link>
